4359c06f3b8e4b2d6594758d9bd12cf3c8c03f7e
fahruldengo <142699753+fahruldengo@users.noreply.github.com>
2026-06-14 23:58:47 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Konfigurasi. Isi API_URL dengan URL Web App Apps Script Anda,
 // atau biarkan kosong dan sambungkan lewat layar Setup.
 window.CONFIG = {
-  API_URL: "",            // contoh: "https://script.google.com/macros/s/XXXX/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbw0pFE2_LI8eoocMFjEsa1wusoJlw1YaAggWXn4ldVWC3kfKv8VE_jIUy1gBL9sh_U2/exec",            // contoh: "https://script.google.com/macros/s/XXXX/exec"
   DEFAULT_YEAR: 2026
 };
