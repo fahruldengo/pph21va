a24894befece71a1c0f05defd3b2e7b767c3e3be
fahruldengo <142699753+fahruldengo@users.noreply.github.com>
2026-06-14 23:44:00 +0800

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// ====== KONFIGURASI ======
-// Ganti dengan URL Web App Apps Script kamu (yang berakhiran /exec)
-window.PPH_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbxUeaUj8ODo1XhFNVNtiV0NXTllCp36ahepqmC84aYiqqWbeXjkR-M6SV7SVwDRcfAT/exec'
-};
